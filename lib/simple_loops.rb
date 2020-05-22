65a81825b9b15b3ccb0b50bb63d9776bbef8440e
Roan Bascoe <roanbascoe5@gmail.com>
2020-05-22 22:02:38 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,6 +1,10 @@
 # Write your methods here
-def loop_message_five_times("Hello World.")
-    puts "Hello World." * 5
+def loop_message_five_times(message)
+    count = 0 
+  while count < 5 do
+    puts "Hello World." 
+    count += 1 
+  end
 end 
 
 def loop_message_n_times

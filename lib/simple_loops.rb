e6c57853d19d8f6dc8cce9f8ee4b6018cc7c50e0
Roan Bascoe <roanbascoe5@gmail.com>
2020-05-22 22:22:14 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -30,7 +30,7 @@ def output_array(array)
 end
 
 output_array("Be yourself;", "everyone else is already taken", "- Oscar Wilde")
-output_array()
+output_array(5, 4, 3, 2, 1)
 
 def return_string_array
   
